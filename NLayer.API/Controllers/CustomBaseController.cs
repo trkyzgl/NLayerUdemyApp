48ba4a4217253a384403b98907397014ca92cb66
Tarık Yüzgül <trkyzgl@gmail.com>
2024-05-04 01:17:12 +0300

    API katmanında ProductsController oluşturuldu. Controller larda yapılan çalışmaları ayarlamak için CustomBaseController oluşturuldu.

---
+++ NLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+
+        /*Bu Controller ı oluşturmamızdaki Temel amaç ; Endpointlerimizde hem Ok, Created gibi durum kodlarını, hemde 204,201 gibi durum kodlarını yazmak yerine 
+         sadece 204,201,500 gibi durum kodlarını method içinde belirtmektir*/
+
+        [NonAction] // Bu method bir endpoint değildir.
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if (response.StatusCode == 204)
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+            // değilse
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+
+
+        }
+
+    }
+}
---
+++ NLayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]    /* CustomControllerBase de olduğu için pasif oalcak */
+    [ApiController]           /* CustomControllerBase de olduğu için pasif oalcak */
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductsController(IMapper mapper, IService<Product> service)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+
+
+        [HttpGet]    // Bu bir HttpGet isteği olacak
+        public async Task<IActionResult> All()
+        {
+            var products = await _service.GetAllAsync();
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+            //return Ok(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));  // BaseControllerde yazdığımız kodlar ile bir daha 'Ok' gibi durumlar dönmeye gerek yok
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
+        }
+
+
+        //www.mysite/api/products/5     gibi olacak
+        [HttpGet("id")]    // Bu bir HttpGet iisteği olacak
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            var productsDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productsDto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            var productsDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(201, productsDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDto));
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+        }
+
+        // DELETE api/products/5 
+        [HttpDelete("id")]    // Bu bir HttpDelete iisteği olacak
+        public async Task<IActionResult> Remove(int id)
+        {
+
+            var product = await _service.GetByIdAsync(id);
+
+
+            await _service.RemoveAsync(product);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(204));
+        }
+    }
+}
